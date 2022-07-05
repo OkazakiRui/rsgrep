a0d23d97c6ac5fab45b2c3c81f5049c90c73fc10
OkazakiRui <okazaki200226@gmail.com>
2022-07-05 15:28:30 +0900

    feat: cat main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::read_to_string;
+
+fn run_cat() {
+    let path = "./src/main.rs";
+    match read_to_string(path) {
+        Ok(contents) => println!("{}", contents),
+        Err(e) => println!("{}", e),
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    run_cat();
 }
